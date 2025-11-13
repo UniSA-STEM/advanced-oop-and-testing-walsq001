75423b2230812a0e7846ce31e4c8a473cf00fc61
Scott Walker <walsq001@mymail.unisa.edu.au>
2025-11-13 18:43:13 +1030

    Testing Lion instantiation via main.py - Develop while loop - Testing resulted in IndexError being raised prior to finishing scope of test - Introduced 'and' statement into while loop to validate there are available names

--- animal.py
+++ animal.py
@@ -10,7 +10,7 @@ import random
 
 class Animal:
     id_counter = 0
-    def __init__(self, name, category, species, diet, habitat):
+    def __init__(self, name, category, species, diet, habitat, sound):
         self.__id = Animal.id_counter
         Animal.id_counter += 1
 
@@ -20,11 +20,13 @@ class Animal:
         self.__age = random.randint(1, 20)
         self.__diet = diet
         self.__habitat = habitat
+        self.__sound = sound
 
     def __str__(self):
         return (f"ID: {self.__id}, Name: {self.__name}, Age: {self.__age}\n"
                 f"Species: {self.__species}, Category: {self.__category}\n"
-                f"Habitat: {self.__habitat}, Diet: {self.__diet}")
+                f"Habitat: {self.__habitat}, Diet: {self.__diet}\n"
+                f"Sound: {self.__sound}")
 
     def sleep(self):
         # TODO: Develop this function, incorporate an attribute
@@ -49,23 +51,59 @@ TODO: Program subclasses underneath each category of animal.
 """
 
 class Mammal(Animal):
-    def __init__(self, name, species, diet, habitat):
-        super().__init__(name,"Mammal", species, diet, habitat)
+    def __init__(self, name, species, diet, habitat, sound):
+        super().__init__(name,"Mammal", species, diet, habitat, sound)
 
 class Lion(Mammal):
+    available_names = ["Simba", "Nala", "Mufasa", "Scar", "Leo"]
     def __init__(self, name=None):
-        names = ["Simba", "Nala", "Mufasa", "Scar", "Leo"]
-        chosen_name = random.choice(names)
+        chosen_name = random.choice(Lion.available_names)
+        Lion.available_names.remove(chosen_name)
         super().__init__(
             chosen_name,
             "Lion",
             "Carnivore",
+            "Savannah",
+            "Roar!"
+        )
+
+class Elephant(Mammal):
+    def __init__(self, name=None):
+        names = ["Elly", "Stampy", "Dumbo", "Bigfoot", "Peanut"]
+        chosen_name = random.choice(names)
+        super().__init__(
+            chosen_name,
+            "Elephant",
+            "Herbivore",
+            "Savannah",
+            "Trumpet"
+        )
+
+class Kangaroo(Mammal):
+    def __init__(self, name=None):
+        names = ["Skippy the Bush Kangaroo", "Hopscotch", "Matilda", "Sheila"]
+        chosen_name = random.choice(names)
+        super().__init__(
+            chosen_name,
+            "Kangaroo",
+            "Herbivore",
             "Savannah"
         )
 
 class Reptile(Animal):
-    def __init__(self):
-        super().__init__(None, None, "Reptile")
+    def __init__(self, name, species, diet, habitat):
+        super().__init__(name, "Reptile", species, diet, habitat)
+
+class Crocodile(Reptile):
+    def __init__(self, name=None):
+        names = ["Ali", "Snappy", "Niblet", "Chomper", "Raptor"]
+        chosen_name = random.choice(names)
+        super().__init__(
+            chosen_name,
+            "Crocodile",
+            "Carnivore",
+            "Wetlands",
+        )
 
 class Bird(Animal):
     def __init__(self):
--- main.py
+++ main.py
@@ -9,7 +9,15 @@ This is my own work as defined by the University's Academic Misconduct Policy.
 
 from animal import Lion
 
-lion = Lion()
-lion2 = Lion()
-print(lion)
-print(lion2)
+lions = []
+count = 0
+
+while count < 6 and Lion.available_names:
+    lion = Lion()
+    lions.append(lion)
+    print(lion)
+    count += 1
+
+print("\nSummary: ")
+for lion in lions:
+    print(f"- {lion._Animal__name}")
